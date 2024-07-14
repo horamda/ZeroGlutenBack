8bc234cb6502b409299cb817f8a8ae2e9016dd65
Horacio Yeuchuk <horamda@gmail.com>
2024-07-14 16:14:31 -0300

    database pythonanyware

--- db.py
+++ db.py
@@ -1,22 +1,42 @@
 import pymysql
 
+#local 
+#def conectarMySQL():
+#    return pymysql.connect(
+#        host='localhost',
+#        user='root',
+#        password='root',
+#        db='final',
+#        charset='utf8mb4',
+#        cursorclass=pymysql.cursors.DictCursor
+#    )
 
+#def conectarRest():
+#    return pymysql.connect(
+#        host='localhost',
+#        user='root',
+#        password='root',
+#        db='final',
+#        charset='utf8mb4',
+#       cursorclass=pymysql.cursors.DictCursor
+#    )
+    
 def conectarMySQL():
     return pymysql.connect(
-        host='localhost',
-        user='root',
-        password='root',
-        db='final',
+        host='horamda.mysql.pythonanywhere-services.com',
+        user='horamda',
+        password='zerogluten',
+        db='horamda$default',
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor
     )
 
 def conectarRest():
     return pymysql.connect(
-        host='localhost',
-        user='root',
-        password='root',
-        db='final',
+        host='horamda.mysql.pythonanywhere-services.com',
+        user='horamda',
+        password='zerogluten',
+        db='horamda$default',
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor
-    )
+    )    
